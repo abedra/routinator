0a20c4837a1a993b963425dfda340878d94a3688
Aaron Bedra <aaron@aaronbedra.com>
2014-09-28 17:56:52 -0500

    Update to 5.5

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Setup a path to fetch packages
-export PKG_PATH=ftp://ftp.openbsd.org/pub/OpenBSD/4.9/packages/i386
+export PKG_PATH=ftp://ftp.openbsd.org/pub/OpenBSD/5.5/packages/i386
 
 # Install essentials
-pkg_add git curl pftop alpine
+pkg_add git curl pftop mutt
 
 # Fetch this repository
 git clone git://github.com/abedra/routinator
@@ -16,8 +16,8 @@ ln -sf routinator/home/script ~/script
 
 # Fetch the OpenBSD Sources
 cd /usr/src
-curl -O ftp://ftp.openbsd.org/pub/OpenBSD/4.9/src.tar.gz
-curl -O ftp://ftp.openbsd.org/pub/OpenBSD/4.9/sys.tar.gz
+curl -O ftp://ftp.openbsd.org/pub/OpenBSD/5.5/src.tar.gz
+curl -O ftp://ftp.openbsd.org/pub/OpenBSD/5.5/sys.tar.gz
 
 # Extract Sources
 tar xzf src.tar.gz
