aea4eddf6c8f6917597713f531b86104458ba1ff
Aaron Bedra <aaron@aaronbedra.com>
2016-05-20 23:56:11 -0500

    Distribute configs to the right places

--- routinator.go
+++ routinator.go
@@ -57,25 +57,48 @@ func createOutputDirectories() {
 
 func writeConfig(config Configuration, inputPath string, outputPath string) {
 	outFile, _ := os.Create(outputPath)
-	t, _ := template.ParseFiles(inputPath)
+	t, err := template.ParseFiles(inputPath)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 	t.Execute(outFile, config)
 }
 
-func writeConfigs(config Configuration) {
-	writeConfig(config, "templates/pf.conf.tmpl", "out/etc/pf.conf")
-	writeConfig(config, "templates/rc.conf.local.tmpl", "out/etc/rc.conf.local")
-	writeConfig(config, "templates/ext_hostname.tmpl", "out/etc/hostname."+config.ExternalInterface)
-	writeConfig(config, "templates/int_hostname.tmpl", "out/etc/hostname."+config.InternalInterface)
-	writeConfig(config, "templates/dhcpd.conf.tmpl", "out/etc/dhcpd.conf")
-	writeConfig(config, "templates/sysctl.conf.tmpl", "out/etc/sysctl.conf")
-	writeConfig(config, "templates/update.tmpl", "out/home/bin/update")
-	writeConfig(config, "templates/recompile_kernel.tmpl", "out/home/bin/recompile_kernel")
-	writeConfig(config, "templates/recompile_system.tmpl", "out/home/bin/recompile_system")
-	writeConfig(config, "templates/.profile.tmpl", "out/home/.profile")
+func writeConfigs(config Configuration, templateDir string) {
+	writeConfig(config, templateDir + "/pf.conf.tmpl", "out/etc/pf.conf")
+	writeConfig(config, templateDir + "/rc.conf.local.tmpl", "out/etc/rc.conf.local")
+	writeConfig(config, templateDir + "/ext_hostname.tmpl", "out/etc/hostname."+config.ExternalInterface)
+	writeConfig(config, templateDir + "/int_hostname.tmpl", "out/etc/hostname."+config.InternalInterface)
+	writeConfig(config, templateDir + "/dhcpd.conf.tmpl", "out/etc/dhcpd.conf")
+	writeConfig(config, templateDir + "/sysctl.conf.tmpl", "out/etc/sysctl.conf")
+	writeConfig(config, templateDir + "/update.tmpl", "out/home/bin/update")
+	writeConfig(config, templateDir + "/recompile_kernel.tmpl", "out/home/bin/recompile_kernel")
+	writeConfig(config, templateDir + "/recompile_system.tmpl", "out/home/bin/recompile_system")
+	writeConfig(config, templateDir + "/.profile.tmpl", "out/home/.profile")
+}
+
+func move(in, out string) {
+	err := os.Rename(in, out)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func moveConfigs(config Configuration) {
+	move("out/home/.profile", "/root/.profile")
+	move("out/home/bin", "/root/bin")
+	move("out/etc/pf.conf", "/etc/pf.conf")
+	move("out/etc/rc.conf.local", "/etc/rc.conf.local")
+	move("out/etc/hostname."+config.ExternalInterface, "/etc/hostname."+config.ExternalInterface)
+	move("out/etc/hostname."+config.InternalInterface, "/etc/hostname."+config.InternalInterface)
+	move("out/etc/dhcpd.conf", "/etc/dhcpd.conf")
+	move("out/etc/sysctl.conf", "/etc/sysctl.conf")
 }
 
 func main() {
 	configPtr := flag.String("config", "firewall.example.json", "Path to config file")
+	templatePtr := flag.String("templates", "templates", "Path to templates")
 	flag.Parse()
 
 	config := readConfiguration(*configPtr)
@@ -83,5 +106,6 @@ func main() {
 	config.VersionString = strings.Join(strings.Split(config.Version, "."), "_")
 
 	createOutputDirectories()
-	writeConfigs(config)
+	writeConfigs(config, *templatePtr)
+	moveConfigs(config)
 }
