84e85e2a4100c2365cf0e9af5f09cb405d6f9529
Aaron Bedra <aaron@aaronbedra.com>
2017-01-14 19:01:11 -0600

    Move to 6.0

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Setup PKG_PATH
-export PKG_PATH=http://mirrors.gigenet.com/pub/OpenBSD/5.9/packages/amd64
+export PKG_PATH=http://mirrors.gigenet.com/pub/OpenBSD/6.0/packages/amd64
 
 # Install essentials
 echo "Installing necessary packages"
-pkg_add rsync-3.1.2 git curl pftop > /dev/null 2>&1
+pkg_add rsync git curl pftop unbound > /dev/null 2>&1
 
 # Fetch the OpenBSD Sources
 echo "Downloading src and sys"
 cd /usr/src
-curl -O -s http://mirrors.gigenet.com/pub/OpenBSD/5.9/src.tar.gz
-curl -O -s http://mirrors.gigenet.com/OpenBSD/5.9/sys.tar.gz
+curl -O -s http://mirrors.gigenet.com/pub/OpenBSD/6.0/src.tar.gz
+curl -O -s http://mirrors.gigenet.com/OpenBSD/6.0/sys.tar.gz
 
 # Extract Sources
 
@@ -28,9 +28,9 @@ echo "Downloading routinator program"
 mkdir setup
 cd setup
 
-curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.1/routinator_openbsd_amd64
-curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.1/firewall.example.json
-curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.1/templates.tar.gz
+curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.3/routinator_openbsd_amd64
+curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.3/firewall.example.json
+curl -O -L -s https://github.com/abedra/routinator/releases/download/0.0.3/templates.tar.gz
 
 chmod +x routinator_openbsd_amd64
 
